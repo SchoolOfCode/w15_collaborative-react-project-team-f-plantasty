19bea49fa3ea5450e04042f78d15b4b31ceb3ffa
Alina Savin <81432036+alinasavin@users.noreply.github.com>
2021-07-02 22:33:10 +0100

    input-added

---
+++ plantasty/src/Components/FullRecipeCard/Method/index.js
@@ -0,0 +1,15 @@
+import React  from 'react';
+
+function Method() {
+   
+        return (
+            <div>
+
+            </div>
+        );
+
+}
+
+
+
+export default Method;
---
+++ plantasty/src/Components/FullRecipeCard/NutritionComponent/index.js
@@ -0,0 +1,15 @@
+import React  from 'react';
+
+function Nutrition() {
+   
+        return (
+            <div>
+
+            </div>
+        );
+
+}
+
+
+
+export default Nutrition;
