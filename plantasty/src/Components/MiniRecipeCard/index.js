510f5837391a22269f022afd462f8a900a51429e
Pillroy <walkertom1996@gmail.com>
2021-07-03 16:13:32 +0100

    ready for API fill the meal plan task

--- plantasty/src/Components/MiniRecipeCard/index.js
+++ plantasty/src/Components/MiniRecipeCard/index.js
@@ -1,40 +1,52 @@
 import IconButton from '../IconButton';
 import styles from './MiniRecipeCard.module.css';
 
-function MiniRecipeCard() {
-
-    return (
-        <div className={styles.mrcWholeContainer}>
-            <div className={styles.mrcSubContainer}>
-                <h3 className={styles.miniRecipeCardHeading}>[insert_recipe_title]</h3>
-                <div className={styles.leftColumn}>
-                    <div className={styles.iconBackground}>
-                        <img className={styles.Icon} src="https://image.shutterstock.com/image-vector/continuous-one-line-drawing-burger-600w-1005965494.jpg"/>
-                    </div>
-                    <div className={styles.iconBackground}>
-                        <img className={styles.Icon} src="https://image.shutterstock.com/image-vector/continuous-one-line-drawing-burger-600w-1005965494.jpg"/>
-                    </div>
-                </div>
-                <div className={styles.middleColumn}>
-                    <img className={styles.recipeImage} src="https://static01.nyt.com/images/2021/01/26/well/well-foods-microbiome/well-foods-microbiome-jumbo.jpg?quality=90"/>
-                </div>
-                <div className={styles.rightColumn}>
-                    <div className={styles.iconBackground}>
-                        <img className={styles.Icon} src="https://image.shutterstock.com/image-vector/continuous-one-line-drawing-burger-600w-1005965494.jpg"/>
-                    </div>
-                    <div className={styles.iconBackground}>
-                        <img className={styles.Icon} src="https://image.shutterstock.com/image-vector/continuous-one-line-drawing-burger-600w-1005965494.jpg"/>
-                    </div>
-                </div>
-                <h4 className={styles.mealType}>
-                    [insert_meal_type]
-                </h4>
-            </div>
-            <div className={styles.mrcSwapContainer}>
-                <button>SWAP</button>
-            </div>
+function MiniRecipeCard(props) {
+  return (
+    <div className={styles.mrcWholeContainer}>
+      <div className={styles.mrcSubContainer}>
+        <h3 className={styles.miniRecipeCardHeading}>{props.text}</h3>
+        <div className={styles.leftColumn}>
+          <div className={styles.iconBackground}>
+            <img
+              className={styles.Icon}
+              src="https://image.shutterstock.com/image-vector/continuous-one-line-drawing-burger-600w-1005965494.jpg"
+            />
+          </div>
+          <div className={styles.iconBackground}>
+            <img
+              className={styles.Icon}
+              src="https://image.shutterstock.com/image-vector/continuous-one-line-drawing-burger-600w-1005965494.jpg"
+            />
+          </div>
         </div>
-    )
+        <div className={styles.middleColumn}>
+          <img
+            className={styles.recipeImage}
+            src="https://static01.nyt.com/images/2021/01/26/well/well-foods-microbiome/well-foods-microbiome-jumbo.jpg?quality=90"
+          />
+        </div>
+        <div className={styles.rightColumn}>
+          <div className={styles.iconBackground}>
+            <img
+              className={styles.Icon}
+              src="https://image.shutterstock.com/image-vector/continuous-one-line-drawing-burger-600w-1005965494.jpg"
+            />
+          </div>
+          <div className={styles.iconBackground}>
+            <img
+              className={styles.Icon}
+              src="https://image.shutterstock.com/image-vector/continuous-one-line-drawing-burger-600w-1005965494.jpg"
+            />
+          </div>
+        </div>
+        <h4 className={styles.mealType}>[insert_meal_type]</h4>
+      </div>
+      <div className={styles.mrcSwapContainer}>
+        <button>SWAP</button>
+      </div>
+    </div>
+  );
 }
 
 export default MiniRecipeCard;
--- plantasty/src/Pages/MealPlanCreated.js
+++ plantasty/src/Pages/MealPlanCreated.js
@@ -1,5 +1,6 @@
 // url  /created
 // can navigate to recipe page to view full recipe - API call
+import { useState } from 'react';
 import Navbar from '../Components/Header/Navbar';
 import Footer from '../Components/Footer';
 import React from 'react';
@@ -10,20 +11,26 @@ import MiniRecipeCard from '../Components/MiniRecipeCard';
 // do the fetch request in a function
 
 function MealPlanCreated() {
-  // const [mealData, setMealData] = useState("");
+  const [mealData, setMealData] = useState('');
 
   function getMealPlan() {
     fetch(
       'https://api.spoonacular.com/mealplanner/generate?apiKey=11cf2295cd61422389f3a0b5611fcb30&timeFrame=week&targetCalories=2000&Diet=Vegan'
     )
       .then((response) => response.json())
-      .then((data) => console.log(data));
+      .then((data) => setMealData(data.week.monday.meals[0].title));
   }
 
   return (
     <div>
       <div>
         <Navbar />
+      </div>
+      <div>
+        <MiniRecipeCard text={mealData} />
+      </div>
+      <div>
+        <MiniRecipeCard />
       </div>
       <div>
         <MiniRecipeCard />
