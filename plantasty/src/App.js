1b958627708d19568c48fb8e7b3b893c960e0e45
Pillroy <walkertom1996@gmail.com>
2021-07-03 13:56:07 +0100

    commented out App.js additions

--- plantasty/src/App.js
+++ plantasty/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import './App.css';
-import RecipePage from './Pages/RecipePage';
+/* import RecipePage from './Pages/RecipePage';
 function App() {
   return (
     <div className="App">
@@ -9,5 +9,5 @@ function App() {
     </div>
   );
 }
-
+*/
 export default App;
