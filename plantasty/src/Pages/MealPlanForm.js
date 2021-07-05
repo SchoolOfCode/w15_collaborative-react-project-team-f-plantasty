10b2326041cd4aeca60198a652b36f87f897adf3
Alina Savin <81432036+alinasavin@users.noreply.github.com>
2021-07-05 16:05:21 +0100

    used useEffect to display recipe image based on meal id

--- plantasty/src/Pages/MealPlanForm.js
+++ plantasty/src/Pages/MealPlanForm.js
@@ -7,13 +7,18 @@ function MealPlanForm() {
   const [mealData, setMealData] = useState(null);
   const [calories, setCalories] = useState(2000);
 
+  //   const AlinaAPI = '11cf2295cd61422389f3a0b5611fcb30';
+  //   const TaniaAPI = '81d1af1612cd4093abbfa7b29f39fd3e';
+  //   const TomAPI = '5b5269dd70b849018665136bf0eb41c9';
+  const AlinaAPI2 = 'ef968f4556ed4b3f880221d46d7bd1b9';
+
   function handleChange(e) {
     setCalories(e.target.value);
   }
 
   function getMealData() {
     fetch(
-      `https://api.spoonacular.com/mealplanner/generate?apiKey=ef968f4556ed4b3f880221d46d7bd1b9&timeFrame=day&targetCalories=${calories}&Diet=Vegan`
+      `https://api.spoonacular.com/mealplanner/generate?apiKey=${AlinaAPI2}&timeFrame=day&targetCalories=${calories}&Diet=Vegan`
     )
       .then((response) => response.json())
       .then((data) => {
@@ -32,7 +37,7 @@ function MealPlanForm() {
       <section className="controls">
         <input type="number" placeholder="Calories" onChange={handleChange} />
       </section>
-      <button onClick={getMealData}>Get Daily Plan</button>
+      <button onClick={getMealData}> Get daily plan</button>
       {mealData && <MealList mealData={mealData} />}
       <div>
         <footer>
