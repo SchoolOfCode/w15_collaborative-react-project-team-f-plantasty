597903dbbde46b54ad412e46ce4f768c7189e921
Alina Savin <81432036+alinasavin@users.noreply.github.com>
2021-07-05 22:15:57 +0100

    solved error

--- plantasty/src/Components/SingleChoiceForm/index.js
+++ plantasty/src/Components/SingleChoiceForm/index.js
@@ -1,23 +1,17 @@
 // single choice
 
-import React from 'react'
+import React from 'react';
 
 function SingleChoiceForm() {
-  
-        return (
-            <section>
-            <div>
-                <h1>Welcome to the weekly meal planner</h1>
-                <h2>Select your preferences bellow</h2>
-            </div>
-            <div>
-                
-            </div>
-            </section>
-        )
-    
+  return (
+    <section>
+      <div>
+        <h1>Welcome to the weekly meal planner</h1>
+        <h2>Select your preferences bellow</h2>
+      </div>
+      <div></div>
+    </section>
+  );
 }
-
-
 
 export default SingleChoiceForm;
