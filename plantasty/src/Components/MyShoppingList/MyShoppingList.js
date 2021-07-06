1313cd5ac6a20123160d121b4023ee5a9f68f533
Maryam Dar <maryamdar@hotmail.com>
2021-07-06 21:36:01 +0100

    ccreated inital MyShoppingList component

---
+++ plantasty/src/Components/MyShoppingList/MyShoppingList.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function ShoppingList () {
+    return (
+        <div className= "Shopping_List_Container">
+
+            <div className= "Shopping_List_Title">
+                <h1> My Shopping List</h1>
+            </div>
+
+        </div>
+
+    
+    )
+}
+
+export default ShoppingList;
