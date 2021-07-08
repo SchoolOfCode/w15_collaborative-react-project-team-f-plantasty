2a4a57fd9d6d1d6c2af0ff0f2df10d98cb2cba05
Alina Savin <81432036+alinasavin@users.noreply.github.com>
2021-07-08 20:41:14 +0100

    added lolo

--- plantasty/src/Components/Hero/index.js
+++ plantasty/src/Components/Hero/index.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import styles from './Hero.module.css';
+import logo from '../../img/logo.png';
 
 function Hero() {
   return (
     <main className={styles}>
       <div>
-        <h1>THIS IS THE HERO</h1>
-        <h2>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        </h2>
+        <h1>Welcome to Plantasty</h1>
+        <h2></h2>
       </div>
       <div>
         <img
           className={styles.imgHero}
-          src="https://images.unsplash.com/photo-1538472861466-364b23ad4286?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
+          // src="https://images.unsplash.com/photo-
+          // 1538472861466-364b23ad4286?ixid=MnwxMjA3fDB8MHxwaG90by
+          // 1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
+          src={logo}
           alt="vegetarian-meal"
         />
       </div>
