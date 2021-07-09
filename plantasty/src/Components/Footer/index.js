96de09d8d4dec64623160be8cf82011c5bf0306d
Maryam Dar <maryamdar@hotmail.com>
2021-07-09 14:11:13 +0100

    Added text to the homepage

--- plantasty/src/Components/Footer/index.js
+++ plantasty/src/Components/Footer/index.js
@@ -6,25 +6,6 @@ import styles from "./footer.module.css";
 function Footer() {
   return (
     <footer className={styles.footer}>
-    <div class="w3-container w3-padding-32 w3-light-grey w3-center w3-xlarge" </div>
-        <div class="w3-section">
-          <i class="fa fa-facebook-official w3-hover-opacity"></i>
-          <i class="fa fa-instagram w3-hover-opacity"></i>
-          <i class="fa fa-snapchat w3-hover-opacity"></i>
-          <i class="fa fa-pinterest-p w3-hover-opacity"></i>
-          <i class="fa fa-twitter w3-hover-opacity"></i>
-          <i class="fa fa-linkedin w3-hover-opacity"></i>
-        </div>
-        <p class="w3-medium">
-          Powered by{" "}
-          <a
-            href="https://www.w3schools.com/w3css/default.asp"
-            target="_blank"
-            class="w3-hover-text-green"
-          >
-            w3.css
-          </a>
-        </p>
       <ul>
         <li>© 2010 — 2020</li>
         <li>
