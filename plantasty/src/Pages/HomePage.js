6f46f73289a92943f0d2b4fac0d1643f01b11829
Maryam Dar <maryamdar@hotmail.com>
2021-07-09 14:06:41 +0100

    Added text to homepage

--- plantasty/src/Pages/HomePage.js
+++ plantasty/src/Pages/HomePage.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Hero from '../Components/Hero';
-import InfoCard from '../Components/InfoCard';
-import TextButton from '../Components/TextButton';
+import React from "react";
+import { Link } from "react-router-dom";
+import Hero from "../Components/Hero";
+import InfoCard from "../Components/InfoCard";
+import TextButton from "../Components/TextButton";
 
 function HomePage() {
   return (
@@ -12,13 +12,13 @@ function HomePage() {
       </header>
       <section>
         <InfoCard
-          title={'Cut down on meat with our tailored meal plan'}
+          title={"Cut down on meat with our tailored meal plan"}
           text={
-            'Why not start your journey today? No idea what to type here someone help please'
+            "Why not start your journey today? Tell us more about your meal time preferences to create a plan that suits you."
           }
           button={
-            <Link to={'/createplan'}>
-              <TextButton text={'Customise my plan'} />
+            <Link to={"/createplan"}>
+              <TextButton text={"Customise my plan"} />
             </Link>
           }
         />
